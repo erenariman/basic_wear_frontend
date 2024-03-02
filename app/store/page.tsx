8c2835027ea0466c183bc37314e3dc6787120b54
Eren Arıman <arimaneren@gmail.com>
2024-03-02 17:31:23 +0300

    Commit

--- app/store/page.tsx
+++ app/store/page.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 
-
 const Store = async () => {
-    const res = await fetch('http://127.0.0.1:8000/api/store/products/');
+    const res = await fetch('http://127.0.0.1:8000/api/store/products/', {cache: "no-cache"});
     const products = await res.json();
-    console.log(products);
+    console.log(products)
     return (
         <>
             <h1>Products</h1>
             <ul>
-                {products.results.map(product => <li key={product.id}>{product.name}</li>)}
+                {products.results.map(product =>
+                    <li key={product.id}>
+                        <img src={product.image} alt="Product Image"/>
+                        {product.name} ${product.price}
+                    </li>)}
             </ul>
         </>
     );
